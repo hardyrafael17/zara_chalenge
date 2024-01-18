b38ab4ef9e8a907c2fb684d485a56ba8fe22ff73
Hardy Rafael Jimenez Matos <hardy.rafel17@gmail.com>
2024-01-18 16:41:49 +0100

    adding yaml context

--- gatsby-node.ts
+++ gatsby-node.ts
@@ -1,4 +1,38 @@
 // https://stackoverflow.com/questions/63124432/how-do-i-configure-mini-css-extract-plugin-in-gatsby
+const fs = require('fs');
+const yaml = require('js-yaml');
+import path from 'path';
+
+exports.createPages = ({ actions }) => {
+  const { createPage } = actions;
+  const ymlDoc = yaml.load(
+    fs.readFileSync('./pageContent/index.yaml', 'utf-8')
+  );
+  ymlDoc.forEach(
+    (
+      element: { path: string; content: string; links: string },
+      index: number
+    ) => {
+      console.log(element.path, 'element path');
+      let component;
+      if (element.path === '/')
+        component = path.resolve(`./src/pages/index.tsx`);
+      else component = path.resolve(`./src/pages${element.path}.tsx`);
+      console.log(component)
+
+      createPage({
+        path: element.path,
+        component,
+        context: {
+          pageContent: element.content,
+          links: element.links,
+          test: 'context test',
+          pagePath: element.path,
+        },
+      });
+    }
+  );
+};
 exports.onCreateWebpackConfig = (helper) => {
   const { stage, actions, getConfig } = helper;
   if (stage === 'develop' || stage === 'build-javascript') {
